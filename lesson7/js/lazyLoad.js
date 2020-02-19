da6d8fc09006ddf385fbd71bc21302b89ff94827
trevormw20 <trevormw20@gmail.com>
2020-02-18 23:25:12 -0800

    lazy load

---
+++ lesson7/js/lazyLoad.js
@@ -0,0 +1,39 @@
+let images = document.querySelectorAll('[data-src]');
+
+function preloadImage(img){
+  const src = img.getAttribute("data-src");
+  img.onload = () => {
+    img.removeAttribute('data-src');
+  };
+  if(!src){
+    return;
+  }
+
+  img.src = src;
+}
+
+const imgOptions ={
+  threShold: 0,
+  rootMargin: "0px 0px 300px 0px"
+};
+
+if('IntersectionObserver' in window) {
+  const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+    entries.forEach(entry => {
+      if(!entry.isIntersecting){
+        return;
+      } else {
+        preloadImage(entry.target);
+        imgObserver.unobserve(entry.target);
+      }
+    });
+  }, imgOptions);
+
+  images.forEach(image => {
+    imgObserver.observe(image);
+  });
+} else {
+  images.forEach(image => {
+    imgObserver.observe(image);
+  });
+}
