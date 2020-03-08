9fbb3e3c2ff8a1f0d3229f54893842b70fe8c301
trevormw20 <trevormw20@gmail.com>
2020-03-07 22:57:50 -0800

    weather home page

---
+++ lesson9/js/weatherUpdate.js
@@ -0,0 +1,50 @@
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  
+  .then(function (jsonObject) {
+    const towns = jsonObject['towns'];
+    console.table(jsonObject);
+    towns.forEach(town => {
+        let townHolder = document.createElement('section');
+        let townName = document.createElement('h1');
+        let motto = document.createElement('h4');
+        let founded = document.createElement('pre');
+        let population = document.createElement('pre');
+        let rainFall = document.createElement('pre');
+        let townImage = document.createElement('img');
+        let events = document.createElement('h3');   
+        events.setAttribute('style', 'white-space: pre;');
+        
+        townName.textContent = town.name;
+        motto.textContent = town.motto;
+        founded.textContent = 'Year Founded: ' + town.yearFounded;
+        population.textContent = 'Population: ' + town.currentPopulation;
+        rainFall.textContent = 'Annual Rainfall: ' + town.averageRainfall;
+        townImage.setAttribute('src', 'images/' + town.photo);
+        townImage.setAttribute('alt', townName);
+        if (town.events[0] != null)
+            events.textContent += town.events[0];
+        if (town.events[1] != null)
+            events.textContent += '\r\n' + town.events[1];
+        if (town.events[2] != null)
+            events.textContent += '\r\n' + town.events[2];
+        if (town.events[3] != null)
+            events.textContent += '\r\n' + town.events[3];
+
+
+        townHolder.appendChild(townName);
+        townHolder.appendChild(motto);
+        townHolder.appendChild(founded);
+        townHolder.appendChild(population);
+        townHolder.appendChild(rainFall);
+        townHolder.appendChild(townImage);
+        townHolder.appendChild(events);
+
+
+        document.querySelector('div.towns').appendChild(townHolder);
+    });
+  });
