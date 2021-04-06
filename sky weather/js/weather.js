0b4867dde285a45cc948d18f3371f03137f6b74e
trevormw20 <45832143+trevormw20@users.noreply.github.com>
2021-04-05 22:38:51 -0700

    profile update

---
+++ sky weather/js/weather.js
@@ -0,0 +1,70 @@
+  const d = new Date();
+  const todayDayNumber = d.getDay();
+
+  const weekDay = new Array(7);
+  weekDay[0] = "Sunday";
+  weekDay[1] = "Monday";
+  weekDay[2] = "Tuesday";
+  weekDay[3] = "Wednesday";
+  weekDay[4] = "Thursday";
+  weekDay[5] = "Friday";
+  weekDay[6] = "Saturday";
+
+  cityURL = "";
+
+  if (window.location.href.indexOf("preston.html") > -1) {
+    cityURL = "//api.openweathermap.org/data/2.5/forecast?id=5604473&appid=eb0f0966f8ca6774077376063c359fb3&units=imperial";
+  }
+
+  if (window.location.href.indexOf("sodasprings.html") > -1) {
+    cityURL = "//api.openweathermap.org/data/2.5/forecast?zip=83276&appid=eb0f0966f8ca6774077376063c359fb3&units=imperial";
+  } 
+
+  if (window.location.href.indexOf("fishhaven.html") > -1) {
+    cityURL = "//api.openweathermap.org/data/2.5/forecast?zip=83287&appid=eb0f0966f8ca6774077376063c359fb3&units=imperial";
+  } 
+
+  const apiURL = cityURL;
+  fetch(apiURL)
+  .then((response) => response.json())
+  .then((weatherInfo) => {
+    console.log(weatherInfo);
+    document.getElementById("townName").textContent = weatherInfo.city.name;
+
+    let mylist = weatherInfo.list;
+
+    let forecastDayNumber = todayDayNumber;
+
+    document.getElementById('condition').innerText = weatherInfo.list[todayDayNumber].weather[0].description;
+    document.getElementById('highTemp').innerText = weatherInfo.list[todayDayNumber].main.temp;
+    document.getElementById('windChill').innerText = Math.round(35.74 + (0.6215 * weatherInfo.list[todayDayNumber].main.temp) - (35.75 * Math.pow(weatherInfo.list[todayDayNumber].wind.speed, 0.16)) + (0.4275 * weatherInfo.list[todayDayNumber].main.temp * Math.pow(weatherInfo.list[todayDayNumber].wind.speed, 0.16)))
+    document.getElementById('humidity').innerText = weatherInfo.list[todayDayNumber].main.humidity;
+    document.getElementById('windSpeed').innerText = weatherInfo.list[todayDayNumber].wind.speed;
+
+    for (i=0; i<mylist.length; i++) {
+      let time = mylist[i].dt_txt;
+
+      if (time.includes('21:00:00')) {
+        forecastDayNumber += 1;
+        if (forecastDayNumber === 7){forecastDayNumber = 0};
+        let theDayName = document.createElement("p");
+        theDayName.textContent = weekDay[forecastDayNumber];
+
+        let theTemp = document.createElement("p");
+        theTemp.textContent = weatherInfo.list[i].main.temp + "\xB0";
+
+        let iconcode = weatherInfo.list[i].weather[0].icon;
+        let iconPath = "//openweathermap.org/img/w/" + iconcode + ".png";
+        let theIcon = document.createElement("img");
+        theIcon.src=iconPath;
+
+        let theDay = document.createElement("div");
+        theDay.appendChild(theDayName);
+        theDay.appendChild(theTemp);
+        theDay.appendChild(theIcon);
+
+        document.getElementById('weatherForecast').appendChild(theDay);
+      }
+    }
+  }  
+);
